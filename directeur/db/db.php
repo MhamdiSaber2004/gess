0d2c7de162fcbd87bccca5c149d8ddf4d1f8383e
saber <sabersabere2004@gmail.com>
2024-12-28 20:50:35 +0100

    db err

--- directeur/db/db.php
+++ directeur/db/db.php
@@ -2,8 +2,8 @@
 
 $servername = "102.219.176.39";
 $database = "cjmxjvbk_testt";
-$username = "cjmxjvbk_argon_latest";
-$password = "cjmxjvbk_argon_latest";
+$username = "cjmxjvbk";
+$password = "07OC24hk@++#";
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $database);
 // Check connection
