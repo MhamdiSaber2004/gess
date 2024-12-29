b467b2db3754ae60facdf98b1258cd2868a5fe81
saber <sabersabere2004@gmail.com>
2024-12-29 21:10:00 +0100

    gda

--- admin/login.php
+++ admin/login.php
@@ -1,8 +1,8 @@
 <?php
-
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
  session_start();
- error_reporting(0);
-include_once "./controller/controller.php";
+include_once "controller/controller.php";
 
 
 
