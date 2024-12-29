8af0537990b8bbcb414ba363d672389341269d79
saber <sabersabere2004@gmail.com>
2024-12-29 17:44:51 +0100

    gda db

--- admin/db/db.php
+++ admin/db/db.php
@@ -1,18 +1,15 @@
-<?php session_start();
- $servername = "102.219.176.39";
-  $database = "cjmxjvbk_argon_latest";
-  $username = "cjmxjvbk_argon_latest";
- $password = "cjmxjvbk_argon_latest";
-//  $servername = "localhost";
-//  $database = "argon";
-//  $username = "root";
-// $password = "";
+<?php 
+
+$servername = "102.219.176.39";
+$database = "cjmxjvbk_testt";
+$username = "cjmxjvbk";
+$password = "#07OC24hk@++#";
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $database);
 // Check connection
 if ($conn->connect_error) {
 die("Connection failed: " . $conn->connect_error);
 }
-
+?>
 
 
