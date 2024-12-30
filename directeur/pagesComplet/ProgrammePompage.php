84e9cc7766d48dbacc7acf7efe2ac550e5871e49
saber <sabersabere2004@gmail.com>
2024-12-30 17:35:05 +0100

    directeur program pompage

--- directeur/pagesComplet/ProgrammePompage.php
+++ directeur/pagesComplet/ProgrammePompage.php
@@ -17,7 +17,6 @@ if (!isset($_SESSION["idPompiste"])) {
    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>برنامج التوزيع اليومي</title>
-   <link href="./برنامج التوزيع اليومي_files/registreStyle.css" rel="stylesheet">
    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">   <style>
       body {
          font-family: Arial, sans-serif;
