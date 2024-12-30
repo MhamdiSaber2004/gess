4a3fbc40f285b1c1e8b3b67eed935c00b1bb76f2
saber <sabersabere2004@gmail.com>
2024-12-30 16:27:09 +0100

    directeur dette benifique

--- directeur/pages/listedetteBenificaierPI.php
+++ directeur/pages/listedetteBenificaierPI.php
@@ -198,7 +198,19 @@ th {
                 <input type="text" class="d-none" name="otre_non" value="0">
                 <div class="form-group">
                     <label for="exampleFormControlInput1">اسم المنتفع</label>
-                    <input type="text" name="nom_dettes_beneficiaires" class="form-control" >
+                    <select class="form-control" name="nom_dettes_beneficiaires" id="nom_dettes_beneficiaires">
+                        
+                    <?php
+                        $sql = "SELECT * FROM benefique_pi where idGess='$idGess' and active=1 ";
+                        $result = $conn->query($sql);
+                        if ($result->num_rows > 0) {
+                            // output data of each row
+                            while ($row = $result->fetch_assoc()) {
+                                echo '<option value="'.$row['idBenefique'].'">'.$row['nom'].'</option>';
+                            }
+                        }
+                    ?>
+                    </select>
                 </div>
                 <div class="form-group">
                     <label for="exampleFormControlInput1">المبلغ</label>
