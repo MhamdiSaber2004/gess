e61ace62257d0fd9957ff420cea1576deac19364
saber <sabersabere2004@gmail.com>
2024-12-30 16:43:57 +0100

    directeur GDA db

--- directeur/GDA/db000000/db.php
+++ directeur/GDA/db000000/db.php
@@ -1,13 +1,12 @@
-<?php
+<?php 
 
 $servername = "102.219.176.39";
-$database = "cjmxjvbk_argon_latest";
-$username = "cjmxjvbk_argon_latest";
-$password = "cjmxjvbk_argon_latest";
+$database = "cjmxjvbk_testt";
+$username = "cjmxjvbk";
+$password = "#07OC24hk@++#";
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $database);
 // Check connection
 if ($conn->connect_error) {
 die("Connection failed: " . $conn->connect_error);
 }
-
