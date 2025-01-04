bcd030078cae1a7f89479cc1c447eff57eecf428
saber <sabersabere2004@gmail.com>
2025-01-04 14:20:47 +0100

    gess branche

--- directeur/pages/listeBenefiquePI.php
+++ directeur/pages/listeBenefiquePI.php
@@ -23,68 +23,11 @@
                   <h3 class="mb-0">قائمة المنتفعين</h3>
                 </div>
                 <div class="col-4 text-right">
-<!--                  <a href="#!" data-toggle="modal" data-target="#modal-form" class="btn btn-sm btn-primary">منتفعين اخرين </a> 
-<a href="index.php?page=ajoutBenefiquePI"  class="btn btn-sm btn-primary">إضافة منتفع  </a> 
- -->
-
-                  </div>
+                  <a href="index.php?page=ajoutBenefiquePI"  class="btn btn-sm btn-primary">إضافة منتفع  </a>
+                </div>
               </div>
             </div>
             
-
-
-            <div class="row">
-
-
-  <div class="col-md-6">
-      <div class="modal fade" id="modal-form" tabindex="-1" role="dialog" aria-labelledby="modal-form" aria-hidden="true">
-    <div class="modal-dialog modal- modal-dialog-centered modal-sm" role="document">
-        <div class="modal-content">
-        	
-            <div class="modal-body p-0">
-            	
-<div class="card bg-secondary border-0 mb-0">
-    <div class="card-header bg-transparent pb-5">
-        <h3 class=" text-center mt-2 mb-3">إضافة منتفع</h3><br>
-        
-          
-            <div class="btn-wrapper text-center">
-                <a href="index.php?page=listeAttentePI" class="btn btn-neutral btn-icon">
-                    <span class="btn-inner--text">منتفعين قيد الانتظار </span>
-                </a><br><br>
-                <a href="index.php?page=listeDesactivePI" class="btn btn-neutral btn-icon">
-                    <span class="btn-inner--text">منتفعين بحسابات معطلة   </span>
-                </a>
-            </div>
-          
-        
-    </div>
-   
-</div>
-    
-            </div>
-            
-        </div>
-    </div>
-</div>
-
-  </div>
-</div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
             <div class="table-responsive col-12">
            <div id="alert" class="alert alert-<?php echo $_SESSION['messageClass'] ?> alert-dismissible  <?php if($_SESSION['message']!="") echo "show"; else echo "visually-hidden" ; ?>" role="alert" >
