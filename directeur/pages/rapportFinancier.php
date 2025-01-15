b7165ccb225929c3631a5c8de80554af3d48e52d
saber <sabersabere2004@gmail.com>
2025-01-15 15:28:57 +0100

    raport financier

--- directeur/pages/rapportFinancier.php
+++ directeur/pages/rapportFinancier.php
@@ -490,6 +490,13 @@
 </form> 
 
 <script>
+  function printPompiste(areaID){
+    var printContent = document.getElementById(areaID).innerHTML;
+    var originalContent = document.body.innerHTML;
+    document.body.innerHTML = printContent;
+    window.print();
+    document.body.innerHTML = originalContent;
+  }
   <?php
     for($i=0 ; $i<20 ; $i++){
       ?>
