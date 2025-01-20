3cf794cbe49e4a527de6765baf66be0f697c0129
saber <sabersabere2004@gmail.com>
2025-01-20 16:18:15 +0100

    test bank end printRenion publique

--- directeur/pages/printReunionPublique.php
+++ directeur/pages/printReunionPublique.php
@@ -26,65 +26,52 @@
               </div>
             </div>
             <div class="card-body">
-              
+              <form action="">
+                <div class="no-print" id="printDiv">
+                  <br><br>
+                  <small class="d-flex justify-content-start text-black">مجمع التنمية في قطاع الفلاحة و الصيد البحري &nbsp;</small>
+                  <small class="d-flex d-xxl-flex justify-content-start justify-content-xxl-start text-black" > ب :&nbsp;</small>
 
+                  <h1 class="d-flex  justify-content-center "><br>إعلان عن الجلسة العامة<br><br></h1>
+                  <small class="d-flex justify-content-start text-black">يعلن مجلس إدارة مجمع التنمية في قطاع الفلاحة و الصيد البحري بــــ: <input type="text" placeholder="..........................................." style="border:0" name="inp1">  &nbsp;<br></small>
+                  <small class="d-flex justify-content-start text-black">أن الجلسة العامة :<input type="text" placeholder="..........................................." style="border:0" name="inp2"> للمجمع ، ستنعقد يوم :<input type="text" placeholder=".........................................................." style="border:0" name="inp3">، على الساعة :<input type="text" placeholder="......................" style="border:0" name="inp4">  &nbsp;<br></small>
+                  <small class="d-flex justify-content-start text-black">بــ: <input type="text" placeholder="..........................................." style="border:0" name="inp5">  &nbsp;<br></small><br>
+                  <small class="d-flex justify-content-start text-black"> <bold>وذلك للنظر في المواضيع التالية :</bold>  &nbsp;<br></small>
+                  <small class="d-flex justify-content-start text-black">- <input type="text" placeholder="............................................................................................................................................................................................................................." style="border:0; width:96%;" name="inp6">  &nbsp;<br></small>
+                  <small class="d-flex justify-content-start text-black">- <input type="text" placeholder="............................................................................................................................................................................................................................." style="border:0; width:96%;" name="inp7">  &nbsp;<br></small>
+                  <small class="d-flex justify-content-start text-black">- <input type="text" placeholder="............................................................................................................................................................................................................................." style="border:0; width:96%;" name="inp8">  &nbsp;<br></small>
+                  <small class="d-flex justify-content-start text-black">- <input type="text" placeholder="............................................................................................................................................................................................................................." style="border:0; width:96%;" name="inp9">  &nbsp;<br></small>
+                  <small class="d-flex justify-content-start text-black">- <input type="text" placeholder="............................................................................................................................................................................................................................." style="border:0; width:96%;" name="inp10">  &nbsp;<br></small>
+                  <small class="d-flex justify-content-start text-black">- <input type="text" placeholder="............................................................................................................................................................................................................................." style="border:0; width:96%;" name="inp11">  &nbsp;<br></small>
+                  <small class="d-flex justify-content-start text-black">- <input type="text" placeholder="............................................................................................................................................................................................................................." style="border:0; width:96%;" name="inp12">  &nbsp;<br></small>
+                  <small class="d-flex justify-content-start text-black">- <input type="text" placeholder="............................................................................................................................................................................................................................." style="border:0; width:96%;" name="inp13">  &nbsp;<br></small>
+                  <small class="d-flex justify-content-start text-black">- <input type="text" placeholder="............................................................................................................................................................................................................................." style="border:0; width:96%;" name="inp14">  &nbsp;<br></small>
+                  <small class="d-flex justify-content-start text-black">- <input type="text" placeholder="............................................................................................................................................................................................................................." style="border:0; width:96%;" name="inp15">  &nbsp;<br></small>
+                  <small class="d-flex justify-content-start text-black">- <input type="text" placeholder="............................................................................................................................................................................................................................." style="border:0; width:96%;" name="inp16">  &nbsp;<br></small>
+                  <small class="d-flex justify-content-start text-black"><br>  &nbsp;<br></small>
+                  <small class="d-flex justify-content-start text-black">يعتبر هذا الإعلان استدعاء شخصيا لجميع المنخرطين المرسمين بصورة قانونية بدفتر الانخراطات بتاريخ هذا اليوم  &nbsp;<br></small>
 
+                  <p class="d-xxl-flex justify-content-xxl-start text-black" ></p>
 
-
-<div class="no-print" id="printDiv">
-    <br><br>
-    <small class="d-flex justify-content-start text-black">مجمع التنمية في قطاع الفلاحة و الصيد البحري &nbsp;</small>
-    <small class="d-flex d-xxl-flex justify-content-start justify-content-xxl-start text-black" > ب :&nbsp;</small>
-
-    <h1 class="d-flex  justify-content-center "><br>إعلان عن الجلسة العامة<br><br></h1>
-    <small class="d-flex justify-content-start text-black">يعلن مجلس إدارة مجمع التنمية في قطاع الفلاحة و الصيد البحري بــــ: <input type="text" placeholder="..........................................." style="border:0">  &nbsp;<br></small>
-    <small class="d-flex justify-content-start text-black">أن الجلسة العامة :<input type="text" placeholder="..........................................." style="border:0"> للمجمع ، ستنعقد يوم :<input type="text" placeholder=".........................................................." style="border:0">، على الساعة :<input type="text" placeholder="......................" style="border:0">  &nbsp;<br></small>
-    <small class="d-flex justify-content-start text-black">بــ: <input type="text" placeholder="..........................................." style="border:0">  &nbsp;<br></small><br>
-    <small class="d-flex justify-content-start text-black"> <bold>وذلك للنظر في المواضيع التالية :</bold>  &nbsp;<br></small>
-    <small class="d-flex justify-content-start text-black">- <input type="text" placeholder="............................................................................................................................................................................................................................." style="border:0; width:96%;" >  &nbsp;<br></small>
-    <small class="d-flex justify-content-start text-black">- <input type="text" placeholder="............................................................................................................................................................................................................................." style="border:0; width:96%;" >  &nbsp;<br></small>
-    <small class="d-flex justify-content-start text-black">- <input type="text" placeholder="............................................................................................................................................................................................................................." style="border:0; width:96%;" >  &nbsp;<br></small>
-    <small class="d-flex justify-content-start text-black">- <input type="text" placeholder="............................................................................................................................................................................................................................." style="border:0; width:96%;" >  &nbsp;<br></small>
-    <small class="d-flex justify-content-start text-black">- <input type="text" placeholder="............................................................................................................................................................................................................................." style="border:0; width:96%;" >  &nbsp;<br></small>
-    <small class="d-flex justify-content-start text-black">- <input type="text" placeholder="............................................................................................................................................................................................................................." style="border:0; width:96%;" >  &nbsp;<br></small>
-    <small class="d-flex justify-content-start text-black">- <input type="text" placeholder="............................................................................................................................................................................................................................." style="border:0; width:96%;" >  &nbsp;<br></small>
-    <small class="d-flex justify-content-start text-black">- <input type="text" placeholder="............................................................................................................................................................................................................................." style="border:0; width:96%;" >  &nbsp;<br></small>
-    <small class="d-flex justify-content-start text-black">- <input type="text" placeholder="............................................................................................................................................................................................................................." style="border:0; width:96%;" >  &nbsp;<br></small>
-    <small class="d-flex justify-content-start text-black">- <input type="text" placeholder="............................................................................................................................................................................................................................." style="border:0; width:96%;" >  &nbsp;<br></small>
-    <small class="d-flex justify-content-start text-black">- <input type="text" placeholder="............................................................................................................................................................................................................................." style="border:0; width:96%;" >  &nbsp;<br></small>
-    <small class="d-flex justify-content-start text-black"><br>  &nbsp;<br></small>
-    <small class="d-flex justify-content-start text-black">يعتبر هذا الإعلان استدعاء شخصيا لجميع المنخرطين المرسمين بصورة قانونية بدفتر الانخراطات بتاريخ هذا اليوم  &nbsp;<br></small>
-
-    <p class="d-xxl-flex justify-content-xxl-start text-black" ></p>
-
-    <div class="container text-center text-black">
-        <div class="row">
-          <div class="col">
-     
-          </div>
-          <div class="col">
-          رئيس مجلس الإدارة <br>_____________
-          </div>
-          <strong class="col">
-       
-          </strong>
-        </div>
-      </div>
-      <br>
-      <small class="d-flex justify-content-start text-black"> <img src="assets/img/reunion/reunionPublique.png" alt="" style="width:70%;">  &nbsp;<br></small>
-
-   
-    
-</div>
-
-
-
-
-
-
-        </div>
- 
-           
+                  <div class="container text-center text-black">
+                    <div class="row">
+                      <div class="col">
+                
+                      </div>
+                      <div class="col">
+                      رئيس مجلس الإدارة <br>_____________
+                      </div>
+                      <strong class="col">
+                  
+                      </strong>
+                    </div>
+                  </div>
+                  <br>
+                  <small class="d-flex justify-content-start text-black"> <img src="assets/img/reunion/reunionPublique.png" alt="" style="width:70%;">  &nbsp;<br></small>
+                </div>
+                <button type="submit" class="btn btn-success form-control">حفظ</button>
+              </form>
+            </div>
           </div>
 
 
