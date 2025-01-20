d4c3b4087074d869755a8b6bf192b5b0de876cc2
saber <sabersabere2004@gmail.com>
2025-01-20 16:20:29 +0100

    test bank end printRenion publique

--- directeur/pages/printReunionPublique.php
+++ directeur/pages/printReunionPublique.php
@@ -69,7 +69,7 @@
                   <br>
                   <small class="d-flex justify-content-start text-black"> <img src="assets/img/reunion/reunionPublique.png" alt="" style="width:70%;">  &nbsp;<br></small>
                 </div>
-                <button type="submit" class="btn btn-success form-control">حفظ</button>
+                <button type="submit" class="btn btn-success form-control text-center text-light" >حفظ</button>
               </form>
             </div>
           </div>
