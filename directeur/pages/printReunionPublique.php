6ee09b27cd9e4bbca2abb0ac1c2dc7d8ae7a9959
MedKhalil04 <khalil04012004@gmail.com>
2025-01-20 15:33:17 +0100

    final version

--- directeur/pages/printReunionPublique.php
+++ directeur/pages/printReunionPublique.php
@@ -90,11 +90,9 @@
 
           <script>
 function printPompiste(areaID){
-    var printContent = document.getElementById(areaID).innerHTML;
-    var originalContent = document.body.innerHTML;
-    document.body.innerHTML = printContent;
+  
     window.print();
-    document.body.innerHTML = originalContent;
+  
 }
 </script>
 
