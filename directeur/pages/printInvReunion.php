f21143bfd6164ce53cf7c6cc8b1b5a44adf39b5d
saber <sabersabere2004@gmail.com>
2025-01-21 22:07:52 +0100

    istid3aaa ijtime3 3am

--- directeur/pages/printInvReunion.php
+++ directeur/pages/printInvReunion.php
@@ -67,14 +67,14 @@
                         </div>
                     </div>
                     <br>
-                    <div style="border: 2px solid black; border-radius: 22px; margin-bottom: 15px; width : 51vw;">
+                    <div style="border: 2px solid black;margin-bottom: 15px; width : 51vw;">
                         <small class="d-flex">يمكنكم تقديم ترشحكم لعضوية مجلس إدارة المجمع أو لجنة المراقبة الداخلية عن طريق<br></small>
                         <small class="d-flex"> رسالة مضمونة الوصول مع الاعلام قبل يوم <input type="text" placeholder="................................." style="border:0"> كما يمكنكم الاطلاع على<br></small>
                         <small class="d-flex   ">  تقارير مجلس اإلدارة والموازنة المالية لسنة <input type="text" placeholder="................................." style="border:0"> ابتداء من يوم <br></small>
                         <small class="d-flex   "><input type="text" placeholder="................................." style="border:0"> وذلك بمقر المجمع الكائن ب   <input type="text" placeholder="................................." style="border:0"><br></small>
                     </div>
 
-                    <div style="border: 2px solid black; border-radius: 22px; margin-bottom: 15px;width : 51vw;">
+                    <div style="border: 2px solid black; margin-bottom: 15px;width : 51vw;">
                         <small class="d-flex   ">اني الممضي اسفله <input type="text" placeholder="................................." style="border:0"> أشهد بأني اتصلت يوم <input type="text" placeholder="................................." style="border:0">بالاستدعاء وبجدول<br></small>
                         <small class="d-flex   "> أعمال الجلسة العامة التي ستنعقد يوم <input type="text" placeholder="................................." style="border:0"> كما يمكنكم الاطلاع على<br></small>
                         <small class="d-flex   ">  تقارير مجلس اإلدارة والموازنة المالية لسنة <input type="text" placeholder="................................." style="border:0"> على الساعة <input type="text" placeholder="................................." style="border:0"> بـ<input type="text" placeholder="................................." style="border:0"><br></small><br>
