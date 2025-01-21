788dc4f5ce43a3e314e08d2b096b27e35a07ee08
saber <sabersabere2004@gmail.com>
2025-01-21 22:52:40 +0100

    istid3aaa ijtime3 3am

--- directeur/pages/printEtatFinance.php
+++ directeur/pages/printEtatFinance.php
@@ -41,7 +41,7 @@
       &nbsp;</small>
 
     <h1 class="d-flex  justify-content-center "><br> كشف عن الوضعية المالية للمجمع  <br><br></h1>
-    <small class="d-flex justify-content-start text-black">من الفترة : <input type="text" placeholder="..........................................." style="border:0" name="inp1">الى <input type="text" placeholder="..........................................." style="border:0" name="inp2">  &nbsp;<br></small>
+    <small class="d-flex justify-content-start text-black">من الفترة : <input type="text" value="0" style="border:0" name="inp1">الى <input type="text" value="0" style="border:0" name="inp2">  &nbsp;<br></small>
 
     <table style="border: 1px solid;width:70%">
       <tr style="border: 1px solid;">
@@ -50,15 +50,15 @@
       </tr>
       <tr style="border: 1px solid;">
                 <td  style="border: 1px solid;">الصندوق</td>
-                <td  style="border: 1px solid;"><input type="text" placeholder="..........................................." style="border:0" name="inp18"></td>
+                <td  style="border: 1px solid;"><input type="text" value="0" style="border:0" name="inp18"></td>
       </tr>
       <tr style="border: 1px solid;">
                 <td  style="border: 1px solid;">الحساب الجاري</td>
-                <td  style="border: 1px solid;"><input type="text" placeholder="..........................................." style="border:0" name="inp21"></td>
+                <td  style="border: 1px solid;"><input type="text" value="0" style="border:0" name="inp21"></td>
       </tr>
       <tr style="border: 1px solid;">
                 <td  style="border: 1px solid;">الرصيد الجملي في أول السنة</td>
-                <td  style="border: 1px solid;"><input type="text" placeholder="..........................................." style="border:0" name="inp24"></td>
+                <td  style="border: 1px solid;"><input type="text" value="0" style="border:0" name="inp24"></td>
       </tr>
     </table>
     <br>
@@ -68,27 +68,27 @@
       </tr>
       <tr style="border: 1px solid;">
                 <td  style="border: 1px solid;">الانخراطات</td>
-                <td  style="border: 1px solid;"><input type="text" placeholder="..........................................." style="border:0" name="inp18" id="in5irat"></td>
+                <td  style="border: 1px solid;"><input type="text" value="0" style="border:0" name="inp18" id="in5irat"></td>
       </tr>
       <tr style="border: 1px solid;">
                 <td  style="border: 1px solid;">الاشتراكات</td>
-                <td  style="border: 1px solid;"><input type="text" placeholder="..........................................." style="border:0" name="inp21" id="ichtirak"></td>
+                <td  style="border: 1px solid;"><input type="text" value="0" style="border:0" name="inp21" id="ichtirak"></td>
       </tr>
       <tr style="border: 1px solid;">
                 <td  style="border: 1px solid;">بيع ماء</td>
-                <td  style="border: 1px solid;"><input type="text" placeholder="..........................................." style="border:0" name="inp24" id="bay3ma"></td>
+                <td  style="border: 1px solid;"><input type="text" value="0" style="border:0" name="inp24" id="bay3ma"></td>
       </tr>
       <tr style="border: 1px solid;">
                 <td  style="border: 1px solid;">مداخيل أخرى</td>
-                <td  style="border: 1px solid;"><input type="text" placeholder="..........................................." style="border:0" name="inp24" id="mada5ilo5ra"></td>
+                <td  style="border: 1px solid;"><input type="text" value="0" style="border:0" name="inp24" id="mada5ilo5ra"></td>
       </tr>
       <tr style="border: 1px solid;">
                 <td  style="border: 1px solid;">مداخيل مختلفة</td>
-                <td  style="border: 1px solid;"><input type="text" placeholder="..........................................." style="border:0" name="inp24" id="mada5ilm5talifa"></td>
+                <td  style="border: 1px solid;"><input type="text" value="0" style="border:0" name="inp24" id="mada5ilm5talifa"></td>
       </tr>
       <tr style="border: 1px solid;">
                 <td  style="border: 1px solid;">مجموع المداخيل</td>
-                <td  style="border: 1px solid;"><input type="text" placeholder="..........................................." style="border:0" name="inp24" id="totalmada5il"></td>
+                <td  style="border: 1px solid;"><input type="text" value="0" style="border:0" name="inp24" id="totalmada5il"></td>
       </tr>
     </table>
     <br>
@@ -98,39 +98,39 @@
       </tr>
       <tr style="border: 1px solid;">
                 <td  style="border: 1px solid;">شراء ماء</td>
-                <td  style="border: 1px solid;"><input type="text" placeholder="..........................................." style="border:0" name="inp18" id="chra2ma"></td>
+                <td  style="border: 1px solid;"><input type="text" value="0" style="border:0" name="inp18" id="chra2ma"></td>
       </tr>
       <tr style="border: 1px solid;">
                 <td  style="border: 1px solid;">مصاريف استغلال أخرى</td>
-                <td  style="border: 1px solid;"><input type="text" placeholder="..........................................." style="border:0" name="inp21" id="isti8lelma"></td>
+                <td  style="border: 1px solid;"><input type="text" value="0" style="border:0" name="inp21" id="isti8lelma"></td>
       </tr>
       <tr style="border: 1px solid;">
                 <td  style="border: 1px solid;">صيانة وإصلاح</td>
-                <td  style="border: 1px solid;"><input type="text" placeholder="..........................................." style="border:0" name="inp24" id="siyana"></td>
+                <td  style="border: 1px solid;"><input type="text" value="0" style="border:0" name="inp24" id="siyana"></td>
       </tr>
       <tr style="border: 1px solid;">
                 <td  style="border: 1px solid;">أجور (مدير فني، عمال)</td>
-                <td  style="border: 1px solid;"><input type="text" placeholder="..........................................." style="border:0" name="inp24" id="2oujour"></td>
+                <td  style="border: 1px solid;"><input type="text" value="0" style="border:0" name="inp24" id="2oujour"></td>
       </tr>
       <tr style="border: 1px solid;">
                 <td  style="border: 1px solid;">إدارة وتصرف</td>
-                <td  style="border: 1px solid;"><input type="text" placeholder="..........................................." style="border:0" name="inp24" id="idara"></td>
+                <td  style="border: 1px solid;"><input type="text" value="0" style="border:0" name="inp24" id="idara"></td>
       </tr>
       <tr style="border: 1px solid;">
                 <td  style="border: 1px solid;">مصاريف مختلفة</td>
-                <td  style="border: 1px solid;"><input type="text" placeholder="..........................................." style="border:0" name="inp24" id="masarifo5ra"></td>
+                <td  style="border: 1px solid;"><input type="text" value="0" style="border:0" name="inp24" id="masarifo5ra"></td>
       </tr>
       <tr style="border: 1px solid;">
                 <td  style="border: 1px solid;">مصاريف الأنشطة الأخرى</td>
-                <td  style="border: 1px solid;"><input type="text" placeholder="..........................................." style="border:0" name="inp24" id="masarifanchta"></td>
+                <td  style="border: 1px solid;"><input type="text" value="0" style="border:0" name="inp24" id="masarifanchta"></td>
       </tr>
       <tr style="border: 1px solid;">
                 <td  style="border: 1px solid;">استشار وتجهيز</td>
-                <td  style="border: 1px solid;"><input type="text" placeholder="..........................................." style="border:0" name="inp24" id="istichara"></td>
+                <td  style="border: 1px solid;"><input type="text" value="0" style="border:0" name="inp24" id="istichara"></td>
       </tr>
       <tr style="border: 1px solid;">
                 <td  style="border: 1px solid;">مجموع المصاريف</td>
-                <td  style="border: 1px solid;"><input type="text" placeholder="..........................................." style="border:0" name="inp24" id="totalmasarif"></td>
+                <td  style="border: 1px solid;"><input type="text" value="0" style="border:0" name="inp24" id="totalmasarif"></td>
       </tr>
     </table>
         <br>
@@ -140,11 +140,11 @@
       </tr>
       <tr style="border: 1px solid;">
                 <td  style="border: 1px solid;">الصندوق</td>
-                <td  style="border: 1px solid;"><input type="text" placeholder="..........................................." style="border:0" name="inp18"></td>
+                <td  style="border: 1px solid;"><input type="text" value="0" style="border:0" name="inp18"></td>
       </tr>
       <tr style="border: 1px solid;">
                 <td  style="border: 1px solid;">الحساب الجاري</td>
-                <td  style="border: 1px solid;"><input type="text" placeholder="..........................................." style="border:0" name="inp21"></td>
+                <td  style="border: 1px solid;"><input type="text" value="0" style="border:0" name="inp21"></td>
       </tr>
     </table>
  
@@ -164,12 +164,12 @@ function printPompiste(areaID){
     window.print();
 }
 
-document.getElementById('mada5ilm5talifa').addEventListener("onchange", function(){
+document.getElementById('mada5ilm5talifa').addEventListener("change", function(){
     var deff=parseFloat(document.getElementById('in5irat').value) + parseFloat(document.getElementById('ichtirak').value)+ parseFloat(document.getElementById('bay3ma').value)+ parseFloat(document.getElementById('mada5ilo5ra').value)+parseFloat( document.getElementById('mada5ilm5talifa').value);
     document.getElementById("totalmada5il").value = deff;
 })
 
-document.getElementById('istichara').addEventListener("onchange", function(){
+document.getElementById('istichara').addEventListener("change", function(){
     var deff=parseFloat(document.getElementById('chra2ma').value) + parseFloat(document.getElementById('isti8lelma').value)+ parseFloat(document.getElementById('siyana').value)+ parseFloat(document.getElementById('2oujour').value)+ parseFloat(document.getElementById('idara').value) + parseFloat(document.getElementById('masarifo5ra').value )+ parseFloat(document.getElementById('idara').value )+ parseFloat(document.getElementById('masarifanchta').value)+ parseFloat(document.getElementById('masarifanchta').value);
     document.getElementById("totalmasarif").value = deff;
 })
