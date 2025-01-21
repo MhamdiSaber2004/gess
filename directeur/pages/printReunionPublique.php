4617e01e51bab0bda68aa7fd56010ae526681012
saber <sabersabere2004@gmail.com>
2025-01-21 12:19:02 +0100

    form test

--- directeur/pages/printReunionPublique.php
+++ directeur/pages/printReunionPublique.php
@@ -55,7 +55,7 @@ print_r($_SESSION);
               </div>
             </div>
             <div class="card-body">
-              <form methode="post">
+              <form methode="post" action="printReunionPublique.php">
                 <div class="no-print" id="printDiv">
                   <br><br>
                   <small class="d-flex justify-content-start text-black">مجمع التنمية في قطاع الفلاحة و الصيد البحري &nbsp;</small>
