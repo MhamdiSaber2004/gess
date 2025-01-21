baada1c928f46dc3c65ac71365935dca83c74de0
saber <sabersabere2004@gmail.com>
2025-01-21 21:26:31 +0100

    ma7ther ijtime3 3am

---
+++ directeur/pages/printInvReunion.php
@@ -0,0 +1,106 @@
+  <!-- Main content -->
+  <div class="main-content">
+
+    <!-- Header -->
+    <div class="header bg-gradient-primary pb-8 pt-5 pt-md-8">
+      <div class="container-fluid">
+        <div class="header-body">
+         
+        </div>
+      </div>
+    </div>
+    <!-- Page content -->
+
+    <div class="container-fluid mt--7">
+      <!-- table -->
+     
+      <div class="col-xl-12 order-xl-1">
+          <div class="card bg-white shadow">
+            <div class="card-header bg-secondary border-0">
+              <div class="row align-items-center no-print">
+                <div class="col-8">
+                  <h3 class="mb-0"><button onclick="printPompiste('printDiv')" class="btn btn-sm btn-primary">طباعة</button> </h3>
+                </div>
+                <div class="col-4 text-right">
+                  <a href="index.php?page=documentsReunions&type=1" class="btn btn-sm btn-primary">رجوع</a>
+                </div>
+              </div>
+            </div>
+            <div class="card-body">
+              <form method="post" action="index.php?page=printReuinionPublique">
+                <div id="printDiv">
+                  <br><br>
+                  <small class="d-flex justify-content-start text-black">مجمع التنمية في قطاع الفلاحة و الصيد البحري &nbsp;</small>
+                  <small class="d-flex d-xxl-flex justify-content-start justify-content-xxl-start text-black" > ب :
+                    <?php
+                      echo  $placeGess;
+                    ?>
+                  &nbsp;</small>
+
+                  <div class="no-print" id="printDiv">
+    <br><br>
+    <small class="d-flex justify-content-start text-black">مجمع التنمية في قطاع الفلاحة و الصيد البحري &nbsp;</small>
+    <small class="d-flex d-xxl-flex justify-content-start justify-content-xxl-start text-black" > ب :&nbsp;</small>
+
+    <h1 class="d-flex  justify-content-center "><br>إستدعاء للجلسة العامة<br><br></h1>
+    <small class="d-flex justify-content-start text-black">      الى السيد : <input type="text" placeholder="..........................................." style="border:0"> رقم بطاقة الانخراط  <input type="text" placeholder="..........................................." style="border:0">&nbsp;<br></small>
+    <h4 class="d-flex  justify-content-center "><br>الموضوع : استدعاء للجلسة العامة<br><br></h4>
+    <h4 class="d-flex  justify-content-center "><br>المصاحيب: جدول أعمال الجلسة العامة<br><br></h4>
+    <h4 class="d-flex  justify-content-center "><br>و بعد، <br><br></h4>
+    <small class="d-flex justify-content-start text-black">يتشرف مجلس إدارة المجمع التنمية في قطاع الفلاحة و الصيد البحري بـ: <input type="text" placeholder="..............................................." style="border:0"> باستدعاكم لحضور الجلسة &nbsp;<br></small><br>
+    <small class="d-flex justify-content-start text"> العامة<input type="text" placeholder="............................................" style="border:0"> التي ستنعقد يوم <input type="text" placeholder=".........................................." style="border:0">على الساعة <input type="text" placeholder="......................................." style="border:0"> &nbsp;<br><br></small>
+    <small class="d-flex justify-content-start text"> بـ<input type="text" placeholder="............................................" style="border:0"> ونظرا للأهمية البالغة التي تكتسيها هذه الجلسة فان حضوركم متأكد جدا. &nbsp; <br><br> </small>
+    
+    
+    <p class="d-xxl-flex justify-content-xxl-start text-black" ></p>
+
+    <div class="container text-center text-black">
+        <div class="row">
+          <div class="col">
+     
+          </div>
+          <div class="col">
+            <input type="text" placeholder="............................................" style="border:0">  في  <input type="text" placeholder="............................................" style="border:0"><br>
+            </div>
+          <div class="col">
+          رئيس مجلس الإدارة <br>_____________
+          </div>
+          <strong class="col">
+       
+          </strong>
+        </div>
+      </div>
+      <br>
+      <div>
+        <small class="d-flex  justify-content-center ">يمكنكم تقديم ترشحكم لعضوية مجلس إدارة المجمع أو لجنة المراقبة الداخلية عن طريق<br></small>
+        <small class="d-flex  justify-content-center "> رسالة مضمونة الوصول مع الاعلام قبل يوم <input type="text" placeholder="................................." style="border:0"> كما يمكنكم الاطلاع على<br></small>
+        <small class="d-flex  justify-content-center ">  تقارير مجلس اإلدارة والموازنة المالية لسنة <input type="text" placeholder="................................." style="border:0"> ابتداء من يوم <br></small>
+        <small class="d-flex  justify-content-center "><input type="text" placeholder="................................." style="border:0"> وذلك بمقر المجمع الكائن ب   <input type="text" placeholder="................................." style="border:0"><br></small>
+      </div>
+
+      <div>
+        <small class="d-flex  justify-content-center ">اني الممضي اسفله <input type="text" placeholder="................................." style="border:0"> أشهد بأني اتصلت يوم <input type="text" placeholder="................................." style="border:0">بالاستدعاء وبجدول<br></small>
+        <small class="d-flex  justify-content-center "> أعمال الجلسة العامة التي ستنعقد يوم <input type="text" placeholder="................................." style="border:0"> كما يمكنكم الاطلاع على<br></small>
+        <small class="d-flex  justify-content-center ">  تقارير مجلس اإلدارة والموازنة المالية لسنة <input type="text" placeholder="................................." style="border:0"> على الساعة <input type="text" placeholder="................................." style="border:0"> بـ<br></small>
+        <small class="d-flex  justify-content-center "><h4>الامضاء</h4><span></span><input type="text" placeholder="................................." style="border:0"> في  <input type="text" placeholder="................................." style="border:0"><br></small>
+      </div>
+      
+
+   
+    
+</div>
+
+              </form>
+            </div>
+          </div>
+          
+<script>
+function printPompiste(areaID){
+    var printContent = document.getElementById(areaID).innerHTML;
+    var originalContent = document.body.innerHTML;
+    window.print();
+    document.body.innerHTML = originalContent;
+}
+</script>
+
+   
