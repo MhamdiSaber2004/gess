97179df863771f21bb8fde8515a64209470a3985
saber <sabersabere2004@gmail.com>
2025-02-03 14:42:26 +0100

    saber bug

--- directeur/topNavBar.php
+++ directeur/topNavBar.php
@@ -1,5 +1,5 @@
     <!-- Top navbar -->
-    <nav class="navbar navbar-top navbar-expand-md navbar-dark" id="navbar-main">
+    <nav class="navbar navbar-top navbar-expand-md navbar-dark no-print" id="navbar-main">
       <div class="container-fluid">
         <!-- Brand -->
         <a class="h4 mb-0 text-white text-uppercase d-none d-lg-inline-block" href="https://a.ness.com.tn/directeur/index.php">اللوحه الرئسية</a>
