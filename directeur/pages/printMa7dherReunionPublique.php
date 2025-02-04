1f0e8ef8f06483dfbb44bf82170c3d93f8511f2a
saber <sabersabere2004@gmail.com>
2025-02-04 11:48:03 +0100

    saber bug

--- directeur/pages/printMa7dherReunionPublique.php
+++ directeur/pages/printMa7dherReunionPublique.php
@@ -15,7 +15,7 @@
      
             <div class="col-xl-12 order-xl-1">
           <div class="card bg-white shadow">
-            <div class="card-header bg-secondary border-0">
+            <div class="card-header bg-secondary border-0 no-print">
               <div class="row align-items-center">
                 <div class="col-8">
                   <h3 class="mb-0"><button onclick="printPompiste('printDiv')" class="btn btn-sm btn-primary">طباعة</button> </h3>
