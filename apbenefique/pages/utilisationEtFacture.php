f1e413448f607b2f8fc39ed404e7e59f2982356b
saber <sabersabere2004@gmail.com>
2025-02-08 12:36:45 +0100

    bennifique

--- apbenefique/pages/utilisationEtFacture.php
+++ apbenefique/pages/utilisationEtFacture.php
@@ -118,7 +118,7 @@
 
     </tr>
     <?php 
-      $sql = "SELECT * FROM `utilisation_et_facture` WHERE idGess = $idGess and moisUF='$moisUF' and activ=1";
+      $sql = "SELECT * FROM `utilisation_et_facture` WHERE idGess = $idGess and activ=1 and idBenifique =$idBenifique ";
       $result = $conn->query($sql);
       $nb=0;
       while ($row = $result->fetch_assoc()) { 
