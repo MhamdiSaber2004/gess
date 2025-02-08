62a4af8e9699e72bd38b4d85acd4fb67f3b1c1a5
saber <sabersabere2004@gmail.com>
2025-02-08 13:38:43 +0100

    bennifique

--- apbenefique/sidebar.php
+++ apbenefique/sidebar.php
@@ -91,7 +91,7 @@
         <h6 class="navbar-heading text-muted">الاعطاب </h6>
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link" href="#" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            <a class="nav-link" href="#" data-bs-toggle="modal" data-bs-target="#problemEtat">
               <i class="ni ni-circle-08 text-blue"></i>تبليغ عن اعطاب
             </a>
           </li>
