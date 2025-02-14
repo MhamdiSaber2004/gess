e9ad47a47499f1ebf560c6eacfe73c2170254161
saber <sabersabere2004@gmail.com>
2025-02-14 15:22:13 +0100

    pompiste

--- appompiste/pages/stationPompage.php
+++ appompiste/pages/stationPompage.php
@@ -42,7 +42,6 @@ if(isset($_GET['moin'])){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>استغلال محطة الضخ الكهربائية</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <style>
         body {
             font-family: Arial, sans-serif;
@@ -273,7 +272,7 @@ if(isset($_GET['moin'])){
         </div>
     </div>
 
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script>
         function printsp(){
             window.print();
